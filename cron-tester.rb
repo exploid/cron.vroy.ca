2a9430d4e14c84ba762e316af1d7e73ad4770c62
Vincent Roy <vincentroy8@gmail.com>
2011-03-25 13:19:23 -0300

    Add support for divisible values, */2 for every two minutes and the like.

--- cron-tester.rb
+++ cron-tester.rb
@@ -13,44 +13,51 @@
 require "rubygems"
 require "time"
 
+# Weekday value in crontab is 0-6 (Sunday=0)
+# Weekday value in Time is 0-6 (Sunday=0)
 def dayofweek_match?(value, time)
   return true if value == "*"
-  return true if value.to_i == time.wday+1
+  return true if value.match(/\*\/(\d+)/) and (time.wday % $1.to_i) == 0
+  return true if value.to_i == time.wday
   return false
 end
 
 def month_match?(value, time)
   return true if value == "*"
+  return true if value.match(/\*\/(\d+)/) and (time.month % $1.to_i) == 0
   return true if value.to_i == time.month
   return false
 end
 
 def dayofmonth_match?(value, time)
   return true if value == "*"
+  return true if value.match(/\*\/(\d+)/) and (time.day % $1.to_i) == 0
   return true if value.to_i == time.day
   return false
 end
 
 def hour_match?(value, time)
   return true if value == "*"
+  return true if value.match(/\*\/(\d+)/) and (time.hour % $1.to_i) == 0
   return true if value.to_i == time.hour
   return false
 end
 
 def minute_match?(value, time)
   return true if value == "*"
+  return true if value.match(/\*\/(\d+)/) and (time.min % $1.to_i) == 0
   return true if value.to_i == time.min
   return false
 end
 
-@cron_line = "2 * * * * /usr/local/bin"
+@cron_line = "0 */3 * * * /usr/local/bin"
 
 @min, @hour, @dayofmonth, @month, @dayofweek, @cmd = @cron_line.split(" ")
 
 @start = Time.parse( Time.now.strftime("%Y-%m-%d %H:%M:00") )
-@end = @start + (3600*36)
 
-while @start < @end
+@matches = []
+while @matches.size < 20
   @start += 60
   
   next unless dayofweek_match?(@dayofweek, @start)
@@ -59,5 +66,7 @@ while @start < @end
   next unless hour_match?(@hour, @start)
   next unless minute_match?(@min, @start)
 
-  puts "#{@start.strftime('%Y-%m-%d %H:%M:%S')}: #{@cmd}"
+  @matches << "#{@start.strftime('%Y-%m-%d %H:%M:%S')}: #{@cmd}"
 end
+
+puts @matches
