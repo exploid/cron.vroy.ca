bb9fdd1a213421c8fee99f57249e56215fb918ee
Vincent Roy <vincentroy8@gmail.com>
2011-04-15 17:35:51 -0300

    Add more validation before running the rest of the loop.

--- classes/cron.rb
+++ classes/cron.rb
@@ -22,7 +22,7 @@ module VRoy
       
       values = cron_string.split(" ")
       
-      raise "Invalid crontab line" if values.size < 6
+      raise "Please provide all required fields." if values.size < 6
 
       @min = values.shift
       @hour = values.shift
@@ -38,8 +38,29 @@ module VRoy
       # else
       # @min, @hour, @dayofmonth, @month, @dayofweek, @cmd = values
       # end
+
+      if !valid?
+        raise errors.join("<br/>")
+      end
     end
     
+    def valid?
+      return errors.empty?
+    end
+    
+    # Runs a quick validation through each type of value and returns an array with all of the error messages.
+    def errors
+      if @errors.nil?
+        @errors = []
+        @errors << "Invalid value for day of week." if !range_match?( @dayofweek, (0..6) )
+        @errors << "Invalid value for month." if !range_match?( @month, (1..12) )
+        @errors << "Invalid value for day of month." if !range_match?( @dayofmonth, (1..31) )
+        @errors << "Invalid value for hour." if !range_match?( @hour, (1..23) )
+        @errors << "Invalid value for minute." if !range_match?( @min, (0..59) )
+      end
+      return @errors
+    end
+
     # Method that confirms if all of the crontab values matches or not.
     def match?(time_obj)
       return false unless dayofweek_match?(@dayofweek, time_obj)
@@ -54,31 +75,26 @@ module VRoy
     # day of week (0 - 6) (Sunday=0)
     # Weekday value in Time is 0-6 (Sunday=0)    
     def dayofweek_match?(cron_value, time_obj)
-      raise "Invalid value for day of week." if !range_match?( cron_value, (0..6) )
       return value_match?(cron_value, time_obj.wday)
     end
 
     # month (1 - 12)
     def month_match?(cron_value, time_obj)
-      raise "Invalid value for month." if !range_match?( cron_value, (1..12) )
       return value_match?(cron_value, time_obj.month)
     end
 
     # day of month (1 - 31)
     def dayofmonth_match?(cron_value, time_obj)
-      raise "Invalid value for day of month." if !range_match?( cron_value, (1..31) )
       return value_match?(cron_value, time_obj.day)
     end
 
     # hour (0 - 23)
     def hour_match?(cron_value, time_obj)
-      raise "Invalid value for hour." if !range_match?( cron_value, (1..23) )
       return value_match?(cron_value, time_obj.hour)
     end
 
     # min (0 - 59)
     def minute_match?(cron_value, time_obj)
-      raise "Invalid value for minute." if !range_match?( cron_value, (0..59) )
       return value_match?(cron_value, time_obj.min)
     end
     
@@ -110,7 +126,7 @@ end
 
 
 __END__
-@cron = VRoy::Cron.new( "*/2 */3 * * * /usr/local/bin" )
+@cron = VRoy::Cron.new( "0 0 0 2 0 /usr/local/bin" )
 
 @start = Time.parse( Time.now.strftime("%Y-%m-%d %H:%M:00") )
 
