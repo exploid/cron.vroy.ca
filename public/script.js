870e14ab0286e1d1d8b50660c3ffea034bea3212
Vincent Roy <vincentroy8@gmail.com>
2011-05-04 07:35:56 -0300

    Add support for a show more button. Missing a loading icon and something that prevents it from doing more than one request at a time.

--- public/script.js
+++ public/script.js
@@ -3,54 +3,72 @@ $(document).ready(function(){
         initField( "#cron", "Enter a crontab line to test and press enter" );
         initField( "#time", "Start time (Optional)" );
 
-        $("#example").click(function() {
-                this.select();
-            });
+        /* Highlight the text in the example input box on click */
+        $("#example").click(function() { this.select(); });
 
-        function postCronLine() {
+        $("#showmore").hide();
+
+        function postCronLine(callback, time) {
             var cron = $("#cron").val();
-            var time = $("#time").val();
+
+            if (time == undefined) {
+                time = $("#time").val();
+            }
             if ( time == "Start time (Optional)" ) {
                 time = "";
             }
+            
 
-            $("#info").html("Processing your request...").show();
-            $("#results").hide();
-            $("#error").hide();
+            if (callback == displayNextCrons) {
+                resetResults();
+                $("#info").html("Processing your request...").show();
+            }
+
             $.ajax({
                     url: "/cron",
                         dataType: "json",
                         data: { cron: cron, time: time },
                         type: "POST",
-                        success: displayNextCrons,
+                        success: callback,
                         error: displayErrorMessage
                         });
         }
         
         function displayErrorMessage() {
+            resetResults();
             $("#error").html("Something wrong happened").show();
-            $("#results").hide();
-            $("#info").hide();
         }
 
         function displayNextCrons( data ) {
+            resetResults();
             if (data.error) {
                 $("#error").html("<div class='invalid_cron'>"+data.invalid_cron+"</div><div>"+data.error+"</div>").show();
-                $("#info").hide();
-                $("#results").hide();
             } else {
-                $("#error").hide();
                 $("#info").html( data.human_format ).show();
+
                 if ( data.custom_time == true ) {
                     $("#time").val( data.start_time );
                 }
 
-                var results = $("#results").html("");
-                for (var i in data.times) {
-                    results.append("<div>"+data.times[i]+"</div>");
-                }
-                results.show();
+                var results = $("#results").html("").show();
+                appendResults( data );
+                $("#showmore").show().focus();
             }
+        }
+
+        function appendResults( data ) {
+            var results = $("#results");
+            for (var i in data.times) {
+                results.append("<div>"+data.times[i]+"</div>");
+            }
+            $("#showmore").focus();
+        }
+
+        function resetResults() {
+            $("#results").html("").hide();
+            $("#info").html("").hide();
+            $("#error").html("").hide();
+            $("#showmore").hide();
         }
 
         function initField(field_selector, value) {
@@ -69,8 +87,21 @@ $(document).ready(function(){
         
             $(field_selector).keyup(function(e) {
                     if ( e.keyCode == 13 ) {
-                        postCronLine();
+                        postCronLine( displayNextCrons );
                     }
                 });
         }
+
+        $("#showmore").click(function() {
+                // Figure out the last date that was shown and increment it for a minute.
+                var time = $("#results div:last").text().match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/);
+                var date = new Date(time[1], parseInt(time[2])-1, parseInt(time[3]), time[4], time[5], time[6], 0);
+                date.setMinutes( date.getMinutes() + 1 );
+
+                // prepare string time for post
+                var strtime = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" ";
+                strtime = strtime + date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+
+                postCronLine( appendResults, strtime );
+            });
 });
